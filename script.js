2ec9efd7472f640d3f7049b2891ac353741c519c
mrdancingcrab <jaederberg@gmail.com>
2025-01-22 14:05:50 +0100

    added send message function

--- script.js
+++ script.js
@@ -16,10 +16,34 @@ async function loadMessages(){
                messageElement.textContent = `${msg.text}`;
                messageContainer.appendChild(messageElement);
            });
-           
+
        } catch (error) {
            console.log(error.message)
        }
 }
 
+async function sendMessage(){
+    const text = messageInput.value.trim();
+
+    if(!text){
+        alert("Please enter a message");
+        return;
+    }
+
+    try {
+        
+        await fetch("https://cloud24chat.azurewebsites.net/api/message", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({text})
+        });
+
+        messageInput.value = "";
+        loadMessages();
+
+    } catch (error) {
+        
+    }
+}
+sendMessageButton.addEventListener("click", sendMessage)
 loadMessages();
